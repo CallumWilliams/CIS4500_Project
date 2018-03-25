e1a15962ec40866888a6d988994164d6bc19ce56
gamora_4500 <gamora_4500@gamora.local>
2018-03-25 17:58:41 -0400

    Basic Google Maps implementation

---
+++ TrickorEatApp/TrickorEatApp/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  TrickorEatApp
+//
+//  Created by gamora_4500 on 2018-03-25.
+//  Copyright © 2018 cis_4500. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: 43.31, longitude: -80.13, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 43.31, longitude: -80.13)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
