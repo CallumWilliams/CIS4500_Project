8b0b9ced0bddd960fc3d3cd82e857f63a6c4d20e
toxin_4500 <toxin_4500@toxin.local>
2018-03-27 16:16:42 -0400

    Email Validation

--- TrickorEatApp/TrickorEatApp/Login.swift
+++ TrickorEatApp/TrickorEatApp/Login.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class Login: UIViewController {
 
+    @IBOutlet weak var EmailField: UITextField!
+    @IBOutlet weak var PasswordField: UITextField!
+    @IBOutlet weak var LoginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        LoginButton.layer.cornerRadius = 8
+        LoginButton.layer.borderWidth = 1
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +26,32 @@ class Login: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func AttemptLogin(_ sender: UIButton) {
+        
+        let login_usr = EmailField.text!
+        let login_pass = PasswordField.text!
+        
+        print(login_usr + " " + login_pass)
+        
+        //validate email structure
+        let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let test = NSPredicate(format: "SELF MATCHES %@", regex)
+        if (test.evaluate(with: login_usr) == false) {
+            print("Invalid")
+            EmailField.textColor = UIColor.red
+        } else {
+            print("Valid")
+            
+            //continue
+            
+            //check backend API
+            
+            //transition
+            
+        }
+        
+    }
+    
     /*
     // MARK: - Navigation
 
